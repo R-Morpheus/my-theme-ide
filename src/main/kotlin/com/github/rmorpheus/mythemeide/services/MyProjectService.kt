0577a3af6e61e38d4e8e9b66fd5143ff184872e1
GitHub Action <action@github.com>
2022-12-27 13:04:23 +0000

    Template cleanup

--- src/main/kotlin/com/github/rmorpheus/mythemeide/services/MyProjectService.kt
+++ src/main/kotlin/com/github/rmorpheus/mythemeide/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.rmorpheus.mythemeide.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.rmorpheus.mythemeide.MyBundle
 
 class MyProjectService(project: Project) {
 
